c64f3f9bf5a9c866f8be2119b83c8a4a22efdadf
John Jacobson <u1201441@notchpeak1.int.chpc.utah.edu>
2024-07-31 10:00:18 -0600

    qry update

--- scripts/gen_table1.py
+++ scripts/gen_table1.py
@@ -13,14 +13,14 @@ def main():
         SELECT
             i.graph as "Input Graph",
             COUNT(DISTINCT i.code) AS "Total Tests",
-            SUM(CASE WHEN hirace.errors > 0 THEN 1 ELSE 0 END) AS "Hirace Races Found",
+            SUM(CASE WHEN hirace.errors > 0 AND i.code NOT LIKE "%boundsBug%" THEN 1 ELSE 0 END) AS "Hirace Races Found",
             SUM(CASE WHEN hirace.errors = 0 AND i.code LIKE "%Bug%" AND i.code NOT LIKE "%boundsBug%" THEN 1 ELSE 0 END) AS "Hirace Races Missed",
-            SUM(CASE WHEN g.errors > 0 THEN 1 ELSE 0 END) AS "iGUARD Races Found",
+            SUM(CASE WHEN g.errors > 0 AND i.code NOT LIKE "%boundsBug%" THEN 1 ELSE 0 END) AS "iGUARD Races Found",
             SUM(CASE WHEN g.errors = 0 AND i.code LIKE "%Bug%" AND i.code NOT LIKE "%boundsBug%" THEN 1 ELSE 0 END) AS "iGUARD Races Missed",
-            SUM(CASE WHEN m.errors > 0 THEN 1 ELSE 0 END) AS "Compute Sanitizer Races Found",
-            SUM(CASE WHEN m.errors = 0 AND i.code LIKE "%Bug%" AND i.code NOT LIKE "%boundsBug%" THEN 1 ELSE 0 END) AS "Compute Sanitizer Races Missed",
-            SUM(CASE WHEN i.errors > 0 THEN 1 ELSE 0 END) AS "Sequential Comparison Races Found",
-            SUM(CASE WHEN i.errors = 0 AND i.code LIKE "%Bug%" AND i.code NOT LIKE "%boundsBug%" THEN 1 ELSE 0 END) AS "Sequential Comparison Races Missed"
+            SUM(CASE WHEN m.errors > 0 AND i.code NOT LIKE "%boundsBug%" THEN 1 ELSE 0 END) AS "Compute Sanitizer\nRaces Found",
+            SUM(CASE WHEN m.errors = 0 AND i.code LIKE "%Bug%" AND i.code NOT LIKE "%boundsBug%" THEN 1 ELSE 0 END) AS "Compute Sanitizer\nRaces Missed",
+            SUM(CASE WHEN i.errors > 0 AND i.code NOT LIKE "%boundsBug%" THEN 1 ELSE 0 END) AS "Sequential Comparison\nRaces Found",
+            SUM(CASE WHEN i.errors = 0 AND i.code LIKE "%Bug%" AND i.code NOT LIKE "%boundsBug%" THEN 1 ELSE 0 END) AS "Sequential Comparison\nRaces Missed"
         FROM indigo i
             INNER JOIN iguard g
                 ON g.code = i.code
@@ -44,7 +44,6 @@ def main():
             i.threads_per_block,
             i.number_of_blocks;
         '''
-        print(query)
         #query = '''
         #SELECT
         #    hirace.graph,
